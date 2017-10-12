c4b332d31cb11364f458c9f02f3c39e9482cee04
ladyvamp <ladyvamp666@gmail.com>
2017-10-13 00:36:03 +0300

    adminpanel: sp, works only save

--- CRUD v3/MainAP.cs
+++ CRUD v3/MainAP.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CRUD_v3
 {
@@ -19,13 +20,53 @@ namespace CRUD_v3
 
         private void MainAP_Load(object sender, EventArgs e)
         {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "searchBaseDataSetTSearchPattern.TSearchPattern". При необходимости она может быть перемещена или удалена.
+            this.tSearchPatternTableAdapter.Fill(this.searchBaseDataSetTSearchPattern.TSearchPattern);
 
         }
 
-        private void btnAdminSP_Click(object sender, EventArgs e)
+        private const string CONNECTION_STRING =
+  "Data Source=DESKTOP-O9H5H8N;Initial Catalog=SearchBase;Integrated Security=True";
+
+        SqlConnection con = new SqlConnection(CONNECTION_STRING);
+
+        private void btnSaveSP_Click(object sender, EventArgs e)
         {
-            AdminSP f = new AdminSP();
-            f.Show();
+            con.Open();
+            string query = "INSERT INTO TSearchPattern (regularExpression, compareWith, action) VALUES ('" + txtRegExp.Text + "','" + cmbCompare.Text + "','" + cmbAction.Text + "')";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            sda.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Запись добавлена");
+        }
+
+        private void btnUpdSP_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "UPDATE TSearchPattern SET regularExpression ='" + txtRegExp.Text + "', compareWith='" + cmbCompare.Text + "', action='" + cmbAction.Text + "' WHERE regularExpression=" + txtRegExp.Text;
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            con.Close();
+            MessageBox.Show("Запись обновлена");
+        }
+
+        private void btnViewSP_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "SELECT * FROM TSearchPattern ORDER BY ID";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            SearchPatternGrid.DataSource = dt;
+            con.Close();
+        }
+
+        private void btnDelSP_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "DELETE FROM TSearchPattern WHERE regularExpression='" + txtRegExp.Text + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            con.Close();
+            MessageBox.Show("Запись удалена");
         }
     }
 }
