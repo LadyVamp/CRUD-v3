5fb8bb510ff59b36b12990f6488899d96af1e5c1
ladyvamp <ladyvamp666@gmail.com>
2017-10-15 22:42:05 +0300

    crud for fileap on video, but an exception is thrown :(

--- CRUD v3/FileAP.cs
+++ CRUD v3/FileAP.cs
@@ -13,205 +13,114 @@ namespace CRUD_v3
 {
     public partial class FileAP : MetroFramework.Forms.MetroForm
     {
-        private const string CONNECTION_STRING =
-"Data Source=DESKTOP-O9H5H8N;Initial Catalog=SearchBase;Integrated Security=True";
+//        private const string CONNECTION_STRING =
+//"Data Source=DESKTOP-O9H5H8N;Initial Catalog=SearchBase;Integrated Security=True";
 
-        SqlConnection con = new SqlConnection(CONNECTION_STRING);
-        SqlCommand cmd;
-        SqlDataAdapter adapter;
-        DataTable dt = new DataTable();
+//        SqlConnection con = new SqlConnection(CONNECTION_STRING);
+//        SqlCommand cmd;
+//        SqlDataAdapter adapter;
+//        DataTable dt = new DataTable();
 
         public FileAP()
         {
             InitializeComponent();
-            FileGrid.ColumnCount = 7;
-            FileGrid.Columns[0].Name = "id";
-            FileGrid.Columns[1].Name = "name";
-            FileGrid.Columns[2].Name = "keywords";
-            FileGrid.Columns[3].Name = "size";
-            FileGrid.Columns[4].Name = "format";
-            FileGrid.Columns[5].Name = "content";
-            FileGrid.Columns[6].Name = "IdCatalog";
-            FileGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            FileGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            FileGrid.MultiSelect = false;
         }
+
+        private async void mtAdd_Click(object sender, EventArgs e)
+        {
+            using(AddEditFile frm = new AddEditFile(new TFile() ))
+            {
+                //frm.ShowDialog();
+                if (frm.ShowDialog()==DialogResult.OK)
+                {
+                    try
+                    {
+                        tFileBindingSource.Add(frm.FileInfo);
+                        db.TFile.Add(frm.FileInfo);
+                        await db.SaveChangesAsync();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        
+        private void mtRefresh_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            tFileBindingSource.DataSource = db.TFile.ToList();
+            tCatalogBindingSource.DataSource = db.TCatalog.ToList();
+            Cursor.Current = Cursors.Default;
+        }
+
+        SearchBaseEntities db;
 
         private void FileAP_Load(object sender, EventArgs e)
         {
-            retrieve();
+            db = new SearchBaseEntities();
+            tFileBindingSource.DataSource = db.TFile.ToList();
+            tCatalogBindingSource.DataSource = db.TCatalog.ToList();
         }
 
-        //Create
-        private void InsertFile(string name, string keywords, string size, string format, string content, string IdCatalog)
+        private async void mtEdit_Click(object sender, EventArgs e)
         {
-            string sql = "INSERT INTO TFile(name,keywords,size, format, content, IdCatalog) VALUES(@name,@keywords,@size, @format, @content, @IdCatalog)";
-            cmd = new SqlCommand(sql, con);
-            cmd.Parameters.AddWithValue("@name", name);
-            cmd.Parameters.AddWithValue("@keywords", keywords);
-            cmd.Parameters.AddWithValue("@size", size);
-            cmd.Parameters.AddWithValue("@format", format);
-            cmd.Parameters.AddWithValue("@content", content);
-            cmd.Parameters.AddWithValue("@IdCatalog", IdCatalog);
-            try
+            TFile obj = tFileBindingSource.Current as TFile;
+            if (obj != null)
             {
-                con.Open();
-                if (cmd.ExecuteNonQuery() > 0)
+                using(AddEditFile frm  = new AddEditFile(obj))
                 {
-                    clearTxtsFile();
-                    MessageBox.Show("Запись добавлена!");
-                }
-                con.Close();
-                retrieve();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                con.Close();
-            }
-        }
-
-        private void populate(String id, String name, string keywords, string size, string format, string content, string IdCatalog)
-        {
-            FileGrid.Rows.Add(id, name, keywords, size, format, content, IdCatalog);
-        }
-
-        private void retrieve()
-        {
-            FileGrid.Rows.Clear();
-            string sql = "SELECT * FROM TFile";
-            cmd = new SqlCommand(sql, con);
-            try
-            {
-                con.Open();
-                adapter = new SqlDataAdapter(cmd);
-                adapter.Fill(dt);
-                foreach (DataRow row in dt.Rows)
-                {
-                    populate(row[0].ToString(), row[1].ToString(), row[2].ToString(), row[3].ToString(), row[4].ToString(), row[5].ToString(), row[6].ToString());
-                }
-                con.Close();
-                dt.Rows.Clear();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                con.Close();
-            }
-        }
-
-        //Update
-        private void UpdateFile(int id, string name, string keywords, string size, string format, string content, string IdCatalog)
-        {
-            string sql = "UPDATE TFile SET name='" + name + "', keywords='" + keywords + "',size='" + size  + "',format='" + format + "',content='" + content + "', IdCatalog='" + IdCatalog + "' WHERE ID=" + id + "";
-            cmd = new SqlCommand(sql, con);
-            try
-            {
-                con.Open();
-                adapter = new SqlDataAdapter(cmd);
-                adapter.UpdateCommand = con.CreateCommand();
-                adapter.UpdateCommand.CommandText = sql;
-                if (adapter.UpdateCommand.ExecuteNonQuery() > 0)
-                {
-                    clearTxtsFile();
-                    MessageBox.Show("Запись обновлена!");
-                }
-                con.Close();
-                retrieve();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                con.Close();
-            }
-        }
-
-        private void DeleteFile(int id)
-        {
-            string sql = "DELETE FROM TFile WHERE ID=" + id + "";
-            cmd = new SqlCommand(sql, con);
-            try
-            {
-                con.Open();
-                MessageBox.Show(con.State.ToString());
-                adapter = new SqlDataAdapter(cmd);
-                adapter.DeleteCommand = con.CreateCommand();
-                adapter.DeleteCommand.CommandText = sql;
-                if (MessageBox.Show("Вы уверены ??", "DELETE", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
-                {
-                    if (cmd.ExecuteNonQuery() > 0)
+                    if(frm.ShowDialog() == DialogResult.OK)
                     {
-                        clearTxtsFile();
-                        MessageBox.Show("Запись удалена!");
+                        try
+                        {
+                            tFileBindingSource.EndEdit();
+                            await db.SaveChangesAsync();
+                        }
+                        catch(Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
                     }
                 }
-                con.Close();
-                retrieve();
             }
+        }
+
+        private void mtDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите удалить запись?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                int rows = FileGrid.RowCount;
+                for (int i = rows-1; i>=0; i--)
+                {
+                    if(FileGrid.Rows[i].Selected)
+                    {
+                        db.TFile.Remove(FileGrid.Rows[i].DataBoundItem as TFile);
+                        tFileBindingSource.RemoveAt(FileGrid.Rows[i].Index);
+                    }
+                }
+            }
+        }
+
+        private async void mtSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(MessageBox.Show("Вы хотите сохранить изменения?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question)==DialogResult.Yes)
+                {
+                    tFileBindingSource.EndEdit();
+                    await db.SaveChangesAsync();
+                }
+            }
+
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
-                con.Close();
-            }
-        }
-
-        private void clearTxtsFile()
-        {
-            txtFileName.Text = "";
-            txtKeywords.Text = "";
-            txtSize.Text = "";
-            cmbFormat.Text = "";
-            txtContent.Text = "";
-            cmbCatalog.Text = "";
-        }
-        //private void FileGrid_MouseClick(object sender, MouseEventArgs e)
-        //{
-        //    txtFileName.Text = FileGrid.SelectedRows[0].Cells[1].Value.ToString();
-        //    txtKeywords.Text = FileGrid.SelectedRows[0].Cells[2].Value.ToString();
-        //    txtSize.Text = FileGrid.SelectedRows[0].Cells[3].Value.ToString();
-        //    cmbFormat.Text = FileGrid.SelectedRows[0].Cells[4].Value.ToString();
-        //    txtContent.Text = FileGrid.SelectedRows[0].Cells[5].Value.ToString();
-        //    cmbCatalog.Text = FileGrid.SelectedRows[0].Cells[6].Value.ToString();
-        //}
-
-
-        private void btnSaveFile_Click(object sender, EventArgs e)
-        {
-            if (txtFileName.Text == "" || txtKeywords.Text == "" || txtSize.Text == "" || cmbFormat.Text == "" || txtContent.Text == "" || cmbCatalog.Text == "")
-            {
-                MessageBox.Show("Поля не заполнены");
-            }
-            else
-            {
-                InsertFile(txtFileName.Text, txtKeywords.Text, txtSize.Text, cmbFormat.Text, txtContent.Text, cmbCatalog.Text);
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
             }
 
         }
-        private void btnViewFile_Click(object sender, EventArgs e)
-        {
-            retrieve();
-        }
-        private void btnUpdFile_Click(object sender, EventArgs e)
-        {
-            if (txtFileName.Text == "" || txtKeywords.Text == "" || txtSize.Text == "" || cmbFormat.Text == "" || txtContent.Text == "" || cmbCatalog.Text == "")
-            {
-                MessageBox.Show("Поля не заполнены");
-            }
-            else
-            {
-                String selected = FileGrid.SelectedRows[0].Cells[0].Value.ToString();
-                int id = Convert.ToInt32(selected);
-                UpdateFile(id, txtFileName.Text, txtKeywords.Text, txtSize.Text, cmbFormat.Text, txtContent.Text, cmbCatalog.Text);
-            }
-
-
-        }
-        private void btnDelFile_Click(object sender, EventArgs e)
-        {
-            String selected = FileGrid.SelectedRows[0].Cells[0].Value.ToString();
-            int id = Convert.ToInt32(selected);
-            DeleteFile(id);
-        }
-
     }
 }
